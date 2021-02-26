6814893e52662447cc2fb0231969c862a4287d6f
yutiansut <yutiansut@qq.com>
2021-02-27 01:38:45 +0800

    #update

--- qifimanager/qifiwebserver.py
+++ qifimanager/qifiwebserver.py
@@ -10,11 +10,12 @@ from qifimanager.manager import QA_QIFIMANAGER, QA_QIFISMANAGER
 class QAQIFI_Handler(QABaseHandler):
     #manager = QA_QIFIMANAGER(mongo_ip)
     manager = QA_QIFISMANAGER(mongo_ip)
+
     def get(self):
         action = self.get_argument('action', 'acchistory')
 
         acc = self.get_argument('account_cookie', 'KTKS_t01_au2012_5min')
-        manage_acc= QA_QIFIMANAGER(acc, mongo_ip)
+        manage_acc = QA_QIFIMANAGER(acc, mongo_ip)
         if action == 'acchistory':
             """
             GET http://127.0.0.1:8019/qifi?action=monthprofit
@@ -40,7 +41,7 @@ class QAQIFI_Handler(QABaseHandler):
                 }
             }
             """
-            history_assets =  manage_acc.assets
+            history_assets = manage_acc.assets
             history_assets.index = history_assets.index.map(str)
             self.write({'res': history_assets.to_dict()})
         elif action == 'monthprofit':
@@ -87,22 +88,23 @@ class QAQIFI_Handler(QABaseHandler):
             }
             """
 
-            res = manage_acc.trade.loc[:, ['commission', 'direction', 
-                'instrument_id', 'offset', 'price', 'trade_date_time', 'volume']].reset_index()
-            res = res.assign(datetime=res.tradetime.map(str)).loc[:, ['commission', 'direction', 
-                'offset', 'price', 'trade_date_time', 'volume', 'code', 'datetime']]
+            res = manage_acc.trade.loc[:, ['commission', 'direction',
+                                           'instrument_id', 'offset', 'price', 'trade_date_time', 'volume']].reset_index()
+            res = res.assign(datetime=res.tradetime.map(str)).loc[:, ['commission', 'direction',
+                                                                      'offset', 'price', 'trade_date_time', 'volume', 'code', 'datetime']]
 
             self.write({'res': QA_util_to_json_from_pandas(res)})
-        
+
         elif action == 'holdingpanel':
-            trading_day =  self.get_argument('trading_day')
+            trading_day = self.get_argument('trading_day')
             res = self.manager.get_holding_panel(acc, trading_day)
             self.write({'res': QA_util_to_json_from_pandas(res)})
 
-            
+
 class QAQIFIS_Handler(QABaseHandler):
     #manager = QA_QIFIMANAGER(mongo_ip)
     manager = QA_QIFISMANAGER(mongo_ip)
+
     def get(self):
         action = self.get_argument('action', 'acchistory')
 
@@ -117,23 +119,26 @@ class QAQIFIS_Handler(QABaseHandler):
             res = self.manager.get_portfolio_panel(portfolio)
 
             self.write({'res': QA_util_to_json_from_pandas(res)})
+
     def post(self):
         action = self.get_argument('action', 'change_name')
         if action == 'drop_account':
 
             account_cookie = self.get_argument('account_cookie')
-            res =  self.manager.drop_account(account_cookie)
+            res = self.manager.drop_account(account_cookie)
             self.write({
                 'res': res,
                 'status': 200
             })
         elif action == 'drop_many':
             account_cookies = self.get_argument('account_cookies')
-            res =  self.manager.drop_many(account_cookies)
+            res = self.manager.drop_many(account_cookies)
             self.write({
                 'res': res,
                 'status': 200
             })
 
+
 if __name__ == "__main__":
-    start_server([(r"/qifi", QAQIFI_Handler), (r"/qifis", QAQIFIS_Handler)], '0.0.0.0', 8019)
+    start_server([(r"/qifi", QAQIFI_Handler),
+                  (r"/qifis", QAQIFIS_Handler)], '0.0.0.0', 8019)
